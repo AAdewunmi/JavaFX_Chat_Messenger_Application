9c867c086e8f27eb1fea037cc4f5351c14109e7b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-09-30 14:04:06 +0100

    Nine Commit: Implement Server JavaFX GUI Control - Label font styling

--- src/main/java/com/application/javafx_chat_messenger_application/HelloApplication.java
+++ src/main/java/com/application/javafx_chat_messenger_application/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 480, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
