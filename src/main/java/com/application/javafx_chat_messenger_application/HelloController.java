1bafc997665d12703cd29e810832d50abf866cd3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-02 12:10:46 +0100

    Eleven Commit: Annotate and Initialize variables in HelloController Class.

--- src/main/java/com/application/javafx_chat_messenger_application/HelloController.java
+++ src/main/java/com/application/javafx_chat_messenger_application/HelloController.java
@@ -1,14 +1,30 @@
 package com.application.javafx_chat_messenger_application;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button_send;
+    @FXML
+    private TextField tf_message;
+    @FXML
+    private VBox vBox_messages;
+    @FXML
+    private ScrollPane sp_main;
+
+    private Server server;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
