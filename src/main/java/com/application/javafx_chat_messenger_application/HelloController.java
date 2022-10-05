311f0cb2c1d05672a2c8d5b465a9504a64a5ecc6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-05 11:54:11 +0100

    Sixteen Commit: Implement EventHandler setOnAction() for Send Button.

--- src/main/java/com/application/javafx_chat_messenger_application/HelloController.java
+++ src/main/java/com/application/javafx_chat_messenger_application/HelloController.java
@@ -2,12 +2,20 @@ package com.application.javafx_chat_messenger_application;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -42,6 +50,35 @@ public class HelloController implements Initializable {
             });
 
             server.receiveMessageFromClient(vBox_messages);
+
+            button_send.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    String messageToSend = tf_message.getText();
+                    if (!messageToSend.isBlank()) {
+                        HBox hBox = new HBox();
+                        hBox.setAlignment(Pos.CENTER_RIGHT);
+                        hBox.setPadding(new Insets(5, 5, 5, 10));
+
+                        Text text = new Text(messageToSend);
+                        TextFlow textFlow = new TextFlow(text);
+
+                        textFlow.setStyle(
+                                "-fx-color: rgb(239, 242, 255)" +
+                                        "-fx-background-color: rgb(15, 125, 242)" +
+                                        "-fx-background-radius: 20px");
+
+                        textFlow.setPadding(new Insets(5, 10, 5, 10));
+                        text.setFill(Color.color(0.934, 0.925, 0.996));
+
+                        hBox.getChildren().add(textFlow);
+                        vBox_messages.getChildren().add(hBox);
+
+                        server.sendMessageToClient(messageToSend);
+                        tf_message.clear();
+                    }
+                }
+            });
     }
 
 }
