1d42ac66994428d9295e9ee60a4d80e068692793
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-08 16:06:01 +0100

    Eighteen Commit: Create Instance Variables for Server Class.

--- src/main/java/com/application/javafx_chat_messenger_application/Server.java
+++ src/main/java/com/application/javafx_chat_messenger_application/Server.java
@@ -1,11 +1,19 @@
 package com.application.javafx_chat_messenger_application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Server Class
  */
 
 public class Server {
 
-
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
 
 }
