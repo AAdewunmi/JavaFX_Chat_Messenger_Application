ac5d86c66b825033c94cd7aa7416b6d80240f32d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-08 16:21:39 +0100

    Nineteen Commit: Create Server Class Constructor.

--- src/main/java/com/application/javafx_chat_messenger_application/Server.java
+++ src/main/java/com/application/javafx_chat_messenger_application/Server.java
@@ -1,7 +1,6 @@
 package com.application.javafx_chat_messenger_application;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,4 +15,16 @@ public class Server {
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
 
+    public Server(ServerSocket serverSocket) {
+        try{
+            this.serverSocket = serverSocket;
+            this.socket = serverSocket.accept();
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        }catch(IOException e){
+            System.out.println("Error creating Server!");
+            e.printStackTrace();
+        }
+
+    }
 }
