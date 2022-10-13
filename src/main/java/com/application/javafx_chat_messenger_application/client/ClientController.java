ea5c6809fafdf616803ca4272584ec4c1eb94551
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-13 15:21:34 +0100

    Thirty Commit: Declare and add @FXML annotation to Variables in ClientController Class.

--- src/main/java/com/application/javafx_chat_messenger_application/client/ClientController.java
+++ src/main/java/com/application/javafx_chat_messenger_application/client/ClientController.java
@@ -1,11 +1,27 @@
 package com.application.javafx_chat_messenger_application.client;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ClientController implements Initializable {
+
+    @FXML
+    private Button button_send;
+    @FXML
+    private TextField tf_message;
+    @FXML
+    private VBox vbox_messages;
+    @FXML
+    private ScrollPane sp_main;
+
+    private Client client;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
