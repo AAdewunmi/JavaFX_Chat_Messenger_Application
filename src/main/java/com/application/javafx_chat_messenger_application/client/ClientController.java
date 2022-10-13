67d1161dd5dfff70d65d2ae33e9ad66b45a694c7
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-13 15:04:33 +0100

    Twenty-Nine Commit: Create Skeleton initialize() method in ClientController Class.

--- src/main/java/com/application/javafx_chat_messenger_application/client/ClientController.java
+++ src/main/java/com/application/javafx_chat_messenger_application/client/ClientController.java
@@ -1,4 +1,13 @@
 package com.application.javafx_chat_messenger_application.client;
 
-public class ClientController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
