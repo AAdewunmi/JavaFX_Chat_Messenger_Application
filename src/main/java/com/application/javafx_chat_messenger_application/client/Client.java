bcd338bffeaf77edf63177f22de4e802d86d0026
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-10-14 08:38:40 +0100

    Thirty-Three Commit: Declare Variables in Client Class.

--- src/main/java/com/application/javafx_chat_messenger_application/client/Client.java
+++ src/main/java/com/application/javafx_chat_messenger_application/client/Client.java
@@ -1,4 +1,14 @@
 package com.application.javafx_chat_messenger_application.client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+
 public class Client {
+
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+
 }
